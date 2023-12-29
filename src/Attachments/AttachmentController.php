19483975a0aedc78c612ac6981a77736f6421247
Mediazz <mathias@mediazz.at>
2023-12-29 12:22:39 +0100

    Update AttachmentController.php

--- src/Attachments/AttachmentController.php
+++ src/Attachments/AttachmentController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Response;
 use Laravel\Nova\Http\Requests\NovaRequest;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
-final readonly class AttachmentController extends Controller
+final class AttachmentController extends Controller
 {
     /** @throws NotFoundHttpException */
     public function destroy(NovaRequest $request): Response
